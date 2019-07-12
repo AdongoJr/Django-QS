0ec9d0baf7d9a6f5553b5b9f65139adae9cf3e3a
AdongoJr <mosezadongo37@gmail.com>
2019-07-12 17:28:34 +0300

    more heroku config

--- qswebapp/settings.py
+++ qswebapp/settings.py
@@ -124,10 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  #new!
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' #new!
 LOGIN_REDIRECT_URL = 'main'
 LOGOUT_REDIRECT_URL = 'home'
 
